cda682a19b67027867c692cb65dd8c10d5f4c6e2
Wellington  Carvalho de Oliveira <wellingtondevops@gmail.com>
2020-10-12 20:27:05 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,58 @@
+const express = require('express')
+const app = express()
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+
+const productRoutes = require('./api/routes/products')
+const ordersRoutes = require('./api/routes/orders')
+
+
+mongoose.connect(
+    process.env.MONGO_URL,
+    {
+        useUnifiedTopology: true,
+        useNewUrlParser: true
+    }
+)
+mongoose.Promise = global.Promise
+
+
+
+app.use(morgan('dev'))
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origen', '*')
+    res.header('Access-Control-Allow-Headers',
+        'Origin, X-Request-With, Content-Type, Accept, Authorization')
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATH, DELETE, GET')
+        return res.status(200).json({})
+    }
+    next()
+})
+
+// Routes which should handle requests
+app.use('/products', productRoutes)
+app.use('/orders', ordersRoutes)
+
+app.use((req, res, next) => {
+    const error = new Error('Not Found')
+    error.status = 404
+    next(error)
+})
+app.use((error, req, res, next) => {
+    res.status(error.status || 500)
+    res.json({
+        error: {
+            message: error.message
+        }
+    })
+})
+
+module.exports = app;
